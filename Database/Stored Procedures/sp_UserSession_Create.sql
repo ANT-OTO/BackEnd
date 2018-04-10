db2775b624381282182ebb6582d5424ff80a287d
Xiang <xiang@ant-oto.com>
2018-04-09 22:02:37 -0400

    no message

---
+++ Database/Stored Procedures/sp_UserSession_Create.sql
@@ -0,0 +1,81 @@
+IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_UserSession_Create]') AND type in (N'P', N'PC'))
+BEGIN
+	print 'Create procedure [dbo].[sp_UserSession_Create] ' + convert(varchar, getdate())
+
+	declare @create as varchar(2048)
+
+	set @create = '
+
+		Create procedure [dbo].[sp_UserSession_Create] 
+		AS 
+		BEGIN 
+			PRINT ''To begin''
+		END
+	'
+
+
+	exec (@create)
+END
+
+print 'Update [sp_UserSession_Create] ... ' + convert(varchar, getdate())
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+Alter procedure [dbo].[sp_UserSession_Create] 
+	@pUserId int,
+	@pExpiredSeconds int,
+	@pToken nvarchar(256) output,
+	@pSystemLanguageId int,
+	@pLastUpdateBy int,
+	@pLastUpdateByType int
+AS
+
+SET NOCOUNT ON
+
+	declare @pTime datetime = getutcdate()
+	
+	update a
+	set a.Expired = 0,
+		a.LastUpdate = @pTime,
+		a.LastUpdateBy = @pLastUpdateBy,
+		a.LastUpdateByType = @pLastUpdateByType
+	from UserSession a 
+	where a.UserId = @pUserId
+	and a.Expired = 0
+
+
+	select @pToken = replace(NEWID(), '-', '')
+
+	insert into UserSession
+	(
+		[UserId],
+		[Token],
+		[SystemLanguageId],
+		[Expired],
+		[ExpireSeconds],
+		[CreateDate],
+		[LastUpdate],
+		[LastUpdateBy],
+		[LastUpdateByType]
+	)
+	select	@pUserId, @pToken, @pSystemLanguageId, 0, @pExpiredSeconds,
+			@pTime, @pTime, @pLastUpdateBy, @pLastUpdateByType
+
+
+
+	
+
+return
+
+/*
+
+*/
+
+
+
+GO
+
+
